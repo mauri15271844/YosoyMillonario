1a95d3519655eefb54ab1aead9dd630cc0751c12
Mauricio herreram <ofmami7@gmail.com>
2022-09-19 19:49:03 -0500

    creacion de interfaz pokemonrepository

--- src/main/java/com/crud/SoyMillonario/model/Pokemon.java
+++ src/main/java/com/crud/SoyMillonario/model/Pokemon.java
@@ -1,4 +1,30 @@
 package com.crud.SoyMillonario.model;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Pokemon {
+
+
+  //----------------atributos------------
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String nombre;
+
+    private int vida;
+
+    private int ataque;
+
+
 }
---
+++ src/main/java/com/crud/SoyMillonario/repository/PokemonRepository.java
@@ -0,0 +1,8 @@
+package com.crud.SoyMillonario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PokemonRepository extends JpaRepository {
+
+
+}
